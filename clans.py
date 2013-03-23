e819f5c8d2dbc9cfd0873259c81397f967c2a0f9
hanrahat <hanrahat@microsoft.com>
2013-03-22 19:42:37 -0700

    Fixed issue #1: Add copyright, license and author infomation

--- clans.py
+++ clans.py
@@ -1,11 +1,28 @@
 # Clan vs Clan - A Deadly Serious Game!
 
+# Copyright 2013 Tom Hanrahan
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License
+
 # Based on the missionaries and cannibals game. Two clans are on one side of a
 # void and must cross safely to the other side.  There is only one vehicle and
 # it can carry only two beings.  One clan is VERY hungry and if at anytime they
 # out number the other clan on either side of the void, they'll eat them.  The
 # goal of the game is to make sure that both clans cross the the other side of
 # the void in an uneaten state.
+
+__version__ = 1.0
 
 import time
 import sys
